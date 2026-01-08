959a074a8bc428378b3806a0847d523be08dca21
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2026-01-08 08:51:19 +0530

    embedding service application file added

---
+++ embedding-service/src/main/java/com/reposync/embedding/EmbeddingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.reposync.embedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddingServiceApplication.class, args);
+    }
+}
+
