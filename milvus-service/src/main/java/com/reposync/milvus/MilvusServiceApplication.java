3d9d93041409382d943322290b01c734305b4679
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2026-01-10 18:50:06 +0530

    milvus service application file added

---
+++ milvus-service/src/main/java/com/reposync/milvus/MilvusServiceApplication.java
@@ -0,0 +1,12 @@
+package com.reposync.milvus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilvusServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MilvusServiceApplication.class, args);
+    }
+}
+
